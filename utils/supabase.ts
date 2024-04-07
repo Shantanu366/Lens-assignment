094ea9a97bf4f364a245f19b427dcd4d03c50861
Shantanu366 <120268245+Shantanu366@users.noreply.github.com>
2024-04-07 14:00:28 +0530

    changes

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,12 +4,3 @@ export default createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
-
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(
-  supabaseUrl as string,
-  supabaseKey as string
-);
